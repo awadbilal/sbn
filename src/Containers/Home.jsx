fa2c4d7950b0105740fd0d1eedf1963eada91c0e
awadbilal <awadbilal99@gmail.com>
2022-02-01 01:47:56 +0300

    Added introduction and prepared grid system

--- src/Containers/Home.jsx
+++ src/Containers/Home.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Categories from "../Components/HomeContent/Categories";
 import Hero from "../Components/HomeContent/Hero";
 import Introduction from "../Components/HomeContent/Introduction";
 
@@ -7,6 +8,7 @@ function Home() {
     <div id="home">
       <Hero />
       <Introduction />
+      <Categories />
     </div>
   );
 }
